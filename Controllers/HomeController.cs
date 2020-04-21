56f362b7d0215eadeebf01d697a7017ec19dab67
Saul Alejandro Aguilar Caballero <saul.aguilar.caballero@gmail.com>
2020-04-21 13:45:33 -0500

    Numero de videos registrados

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MVCLaboratorio.Utilerias;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            //Obtener la información de los videos
+           
+            DataTable dtVideos;
+
+            dtVideos = BaseHelper.ejecutarConsulta("Select * from Video", CommandType.Text);
+
+            ViewData["no_videos"] = dtVideos.Rows.Count;
+           
+
+            return View();
+            
+        }
+
+    }
+}
